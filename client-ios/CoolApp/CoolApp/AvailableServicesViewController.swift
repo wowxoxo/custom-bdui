decd0ac781600d082884284a8ead110df09201f1
wowxoxo <creatsiv@gmail.com>
2024-06-12 10:56:10 +0300

    feat: add some views

---
+++ client-ios/CoolApp/CoolApp/AvailableServicesViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  CoolApp
+//
+//  Created by admin on 09.06.2024.
+//
+
+import UIKit
+
+class AvailableServicesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let services = ["Услуга № 1", "Услуга № 2"]
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ServiceCell")
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return services.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ServiceCell", for: indexPath)
+            cell.textLabel?.text = services[indexPath.row]
+            return cell
+        }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Selected row at \(indexPath.row)")
+            if indexPath.row == 0 {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                if let pointsViewController = storyboard.instantiateViewController(withIdentifier: "custom_bdui_points_view_controller") as? PointsViewController {
+                    print("Navigating to PointsViewController")
+                    navigationController?.pushViewController(pointsViewController, animated: true)
+//                    self.present(pointsViewController, animated: true, completion: nil)
+                } else {
+                    print("Failed to instantiate PointsViewController")
+                }
+            } else {
+                print("Second card clicked")
+                // Handle the second card click
+            }
+        }
+    
+
+}
+
---
+++ client-ios/CoolApp/CoolApp/ViewControllers/PointsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PointsViewController.swift
+//  CoolApp
+//
+//  Created by admin on 12.06.2024.
+//
+
+import UIKit
+
+class PointsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
